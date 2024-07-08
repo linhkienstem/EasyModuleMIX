bf97d8f7f21cdf386fc26ed85e085261ee26914d
Quyet Thanh <thanh139ptit@gmail.com>
2024-07-08 16:16:13 +0700

    change color

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -71,7 +71,7 @@ html_static_path = ['_static']
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
 html_css_files = [
-    'css/custom.css',
+    '_static/custom.css'
 ]
 
 # -- Options for EPUB output
