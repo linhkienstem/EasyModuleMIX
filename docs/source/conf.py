ca7e85488e9712d954008fefdd8e5703cac93a7c
mellivora24 <thanh139ptit@gmai.com>
2024-07-19 16:58:28 +0700

    Change background color

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,7 @@ html_theme_options = {
     'prev_next_buttons_location': 'bottom',
     # 'style_external_links': True,
     # 'vcs_pageview_mode': '',
-    'style_nav_header_background': '#ff7337',
+    # 'style_nav_header_background': '#ff7337',
     # Toc options
 
     'collapse_navigation': True,
